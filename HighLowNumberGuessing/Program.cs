34880bb7e34c0418d831e20c48ec7a57c204f1f9
Peter Baggetta <petebaggetta@gmail.com>
2025-09-04 15:27:35 -0400

    Add project files.

---
+++ HighLowNumberGuessing/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace HighLowNumberGuessing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int GUESS_LIMIT = 7;
+
+            Boolean win = false;
+
+            Console.WriteLine("Welcome to the Number Guessing Game!");
+
+            // Game loops until the player does not want to play anymore
+            while (true)
+            {
+
+                // Generate Random Number from 1 to 100
+                Random rng = new Random();
+                int secretNumber = rng.Next(1, 101);
+
+                Console.WriteLine("\nGuess a number between 1 and 100.");
+
+                for (int i = 0; i < GUESS_LIMIT; i++)
+                {
+                    // Ask for the players guess
+                    Console.WriteLine("\nGuess a number: ");
+                    int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                    if (Math.Abs(secretNumber - userNumber) <= 5 && userNumber != secretNumber)
+                    {
+                        Console.WriteLine("You are close!");
+                    }
+
+                    if (userNumber < secretNumber)
+                    {
+                        // Player guess is too low
+                        Console.WriteLine($"Too low. You have {GUESS_LIMIT - i - 1} guesses left.");
+                        continue;
+
+                    }
+                    if (userNumber > secretNumber)
+                    {
+                        // Player guess is too high
+                        Console.WriteLine($"Too high. You have {GUESS_LIMIT - i - 1} guesses left.");
+                        continue;
+
+                    }
+                    else
+                    {
+                        // Player guesses correct number
+                        Console.WriteLine("You got it!! You Win!!");
+                        win = true;
+                        break;
+                    }
+
+                }
+
+                // Let player know they did not guess the right number in the limited number of tries
+                if (!win)
+                {
+                    Console.WriteLine($"\nYou lose. The correct number is {secretNumber}.\n");
+                }
+
+                // Ask the player if they want to play again
+                Console.WriteLine("Want to play again? (Y/N): ");
+                string playAgain = Console.ReadLine().ToLower();
+                if (playAgain == "n")
+                {
+                    Console.WriteLine("Thank you for playing!");
+                    break;
+                }
+            }
+        }
+    }
+}
